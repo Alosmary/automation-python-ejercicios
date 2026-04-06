a5f70f669d42667a877daa2867f947437add83f5
Alosmary Hernández <aloshdz81@gmail.com>
2026-04-06 18:39:40 -0300

    Primeros ejercicios Python del curso

---
+++ datos_usuarios.py
@@ -0,0 +1,5 @@
+nombre = input("Escribe tu nombre: ")
+edad = input("Escribe tu edad: ")
+profesión = input("Escribe tu profesión: ")
+
+print("Hola", nombre, "tu edad es", edad, "y tu profesión es", profesión)
